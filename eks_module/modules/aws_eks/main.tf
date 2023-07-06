9ab1551debdc7f58c1ae85f7d94d6a70ced82a2a
saikumarpinisetti3 <132870970+saikumarpinisetti3@users.noreply.github.com>
2023-07-06 14:17:02 +0530

    Update main.tf

--- eks_module/modules/aws_eks/main.tf
+++ eks_module/modules/aws_eks/main.tf
@@ -10,7 +10,8 @@ resource "aws_eks_cluster" "eks" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.amazon_eks_cluster_policy
+    aws_iam_role_policy_attachment.amazon_eks_cluster_policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly_EKS
   ]
 
   tags = var.tags
@@ -39,7 +40,43 @@ resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster.name
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly_EKS" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_cluster.name
 }
+
+resource "aws_iam_policy" "eks_cluster_policy" {
+  name        = "eks-cluster-policy"
+  description = "Policy for EKS cluster"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "eks:CreateCluster",
+        "eks:DescribeCluster",
+        "eks:ListNodegroups",
+        "eks:CreateNodegroup",
+        "eks:DescribeNodegroup",
+        "eks:TagResource"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": "iam:PassRole",
+      "Resource": "arn:aws:iam::185869047570:role/eks-cluster1"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy_attachment" "eks_cluster_policy_attachment" {
+  policy_arn = aws_iam_policy.eks_cluster_policy.arn
+  roles      = [aws_iam_role.eks_cluster.name]
+}
+
