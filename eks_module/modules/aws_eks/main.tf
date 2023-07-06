8988a30a2993d216b402d7c1864ef4d44b62de9b
saikumarpinisetti3 <132870970+saikumarpinisetti3@users.noreply.github.com>
2023-07-06 13:49:42 +0530

    Update main.tf

--- eks_module/modules/aws_eks/main.tf
+++ eks_module/modules/aws_eks/main.tf
@@ -59,14 +59,11 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
-  # The ARN of the policy you want to apply
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-
-  # The role the policy should be applied to
-  role = aws_iam_role.eks_cluster.name
+  role       = aws_iam_role.eks_cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
- policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
- role    = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_cluster.name
 }
